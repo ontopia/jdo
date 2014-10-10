29e2151c1809607bba619d74e4732dc52b110686
Quintin Siebers <q.siebers@mssm.nl>
2014-10-10 22:18:58 +0000

    Added default name type creation
    git-svn-id: https://svn.mssm.nl/sandbox/ontopia-jdo@621 869db993-6338-4487-9605-f03c488beb64

--- src/main/java/net/ontopia/topicmaps/impl/jdo/JDOTopicMapBuilder.java
+++ src/main/java/net/ontopia/topicmaps/impl/jdo/JDOTopicMapBuilder.java
@@ -1,3 +1,22 @@
+/*
+ * #!
+ * Ontopia JDO
+ * #-
+ * Copyright (C) 2001 - 2014 The Ontopia Project
+ * #-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * !#
+ */
 
 package net.ontopia.topicmaps.impl.jdo;
 
@@ -14,6 +33,7 @@ import net.ontopia.topicmaps.core.TopicMapBuilderIF;
 import net.ontopia.topicmaps.core.TopicMapIF;
 import net.ontopia.topicmaps.core.TopicNameIF;
 import net.ontopia.topicmaps.core.VariantNameIF;
+import net.ontopia.topicmaps.utils.PSI;
 import net.ontopia.utils.OntopiaRuntimeException;
 
 public class JDOTopicMapBuilder implements TopicMapBuilderIF {
@@ -48,8 +68,13 @@ public class JDOTopicMapBuilder implements TopicMapBuilderIF {
 		return checkAndCast(topic, "Topic", Topic.class).makeTopicName(bntype, value);
 	}
 
-	private Topic getDefaultNameType() {
-		return null; // todo
+	private TopicIF getDefaultNameType() {
+		TopicIF defaultNameType = topicmap.getTopicBySubjectIdentifier(PSI.getSAMNameType());
+		if (defaultNameType == null) {
+			defaultNameType = makeTopic();
+			defaultNameType.addSubjectIdentifier(PSI.getSAMNameType());
+		}
+		return defaultNameType;
 	}
 
 	public VariantNameIF makeVariantName(TopicNameIF name, String value, Collection<TopicIF> scope) {
