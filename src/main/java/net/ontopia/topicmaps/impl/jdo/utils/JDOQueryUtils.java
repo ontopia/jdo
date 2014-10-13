6237c7eff039f9a03a6f194ad9bc90a41e8816bf
Quintin Siebers <q.siebers@mssm.nl>
2014-10-13 11:33:54 +0000

    Added utility class
    git-svn-id: https://svn.mssm.nl/sandbox/ontopia-jdo@641 869db993-6338-4487-9605-f03c488beb64

---
+++ src/main/java/net/ontopia/topicmaps/impl/jdo/utils/JDOQueryUtils.java
@@ -0,0 +1,38 @@
+/*
+ * #!
+ * Ontopia JDO
+ * #-
+ * Copyright (C) 2001 - 2014 The Ontopia Project
+ * #-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * !#
+ */
+
+package net.ontopia.topicmaps.impl.jdo.utils;
+
+import java.util.Collection;
+import java.util.HashSet;
+import javax.jdo.Query;
+
+public class JDOQueryUtils {
+	
+	@SuppressWarnings("unchecked")
+	public static <T> Collection<T> queryToCollection(Query query, Object... parameters) {
+		return (Collection<T>) query.executeWithArray(parameters);
+	}
+
+	public static <T> Collection<T> queryToWrappedSet(Query query, Object... parameters) {
+		return new HashSet<T>(JDOQueryUtils.<T>queryToCollection(query, parameters));
+	}
+
+}
