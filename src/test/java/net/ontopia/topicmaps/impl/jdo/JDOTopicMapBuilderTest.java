b8553ca91333ca1d16b02155711500803efa5853
Quintin Siebers <q.siebers@mssm.nl>
2014-10-16 18:43:31 +0000

    Re-enabled 2 test cases
    git-svn-id: https://svn.mssm.nl/sandbox/ontopia-jdo@703 869db993-6338-4487-9605-f03c488beb64

--- src/test/java/net/ontopia/topicmaps/impl/jdo/JDOTopicMapBuilderTest.java
+++ src/test/java/net/ontopia/topicmaps/impl/jdo/JDOTopicMapBuilderTest.java
@@ -77,21 +77,21 @@ public class JDOTopicMapBuilderTest extends AbstractJDOTest {
 //		builder.makeTopic().addItemIdentifier(URILocator.create("foo:bar"));
 //	}
 	
-//	@Test(expected = ConstraintViolationException.class)
-//	public void testDuplicateII() {
-//		builder.makeTopic().addItemIdentifier(URILocator.create("foo:bar"));
-//		Assert.assertNotNull(topicmap.getObjectByItemIdentifier(URILocator.create("foo:bar")));
-//		builder.makeTopic().addItemIdentifier(URILocator.create("foo:bar"));
-//	}
+	@Test(expected = ConstraintViolationException.class)
+	public void testDuplicateII() {
+		builder.makeTopic().addItemIdentifier(URILocator.create("foo:bar"));
+		Assert.assertNotNull(topicmap.getObjectByItemIdentifier(URILocator.create("foo:bar")));
+		builder.makeTopic().addItemIdentifier(URILocator.create("foo:bar"));
+	}
 	
-//	@Test
-//	public void testAddSameII() {
-//		TopicIF topic = builder.makeTopic();
-//		topic.addItemIdentifier(URILocator.create("foo:bar"));
-//		topic.addItemIdentifier(URILocator.create("foo:bar"));
-//		Assert.assertNotNull(topicmap.getObjectByItemIdentifier(URILocator.create("foo:bar")));
-//		Assert.assertEquals(1, topic.getItemIdentifiers().size());
-//	}
+	@Test
+	public void testAddSameII() {
+		TopicIF topic = builder.makeTopic();
+		topic.addItemIdentifier(URILocator.create("foo:bar"));
+		topic.addItemIdentifier(URILocator.create("foo:bar"));
+		Assert.assertNotNull(topicmap.getObjectByItemIdentifier(URILocator.create("foo:bar")));
+		Assert.assertEquals(1, topic.getItemIdentifiers().size());
+	}
 	
 	@Test
 	public void testAddSameSI() {
