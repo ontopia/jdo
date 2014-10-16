c6f5caf26d3f9ad23edeccd9b3dc569fa2a56fa2
Quintin Siebers <q.siebers@mssm.nl>
2014-10-16 21:02:51 +0000

    Added core.VariantNameTest test cases
    git-svn-id: https://svn.mssm.nl/sandbox/ontopia-jdo@710 869db993-6338-4487-9605-f03c488beb64

---
+++ src/test/java/net/ontopia/topicmaps/impl/jdo/VariantNameTest.java
@@ -0,0 +1,35 @@
+/*
+ * #!
+ * Ontopia JDO
+ * #-
+ * Copyright (C) 2001 - 2014 The Ontopia Project
+ * #-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * !#
+ */
+
+package net.ontopia.topicmaps.impl.jdo;
+
+import net.ontopia.topicmaps.core.TestFactoryIF;
+
+public class VariantNameTest extends net.ontopia.topicmaps.core.VariantNameTest {
+
+	public VariantNameTest(String name) {
+		super(name);
+	}
+	
+	@Override
+	protected TestFactoryIF getFactory() throws Exception {
+		return new JDOTestFactory();
+	}
+}
